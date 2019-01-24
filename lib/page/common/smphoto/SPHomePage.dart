2a99131e81b9f1931f9066b18ca89543044e067a
Nell <dashuai.fu@loncus.com>
2019-01-25 00:23:34 +0800

    继续smphoto

--- lib/page/common/smphoto/SPHomePage.dart
+++ lib/page/common/smphoto/SPHomePage.dart
@@ -1,3 +1,4 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 import 'package:flutter_smallworld/widget/index.dart';
@@ -11,12 +12,14 @@ class SPHomePage extends StatefulWidget {
 }
 
 class _SPHomePageState extends State<SPHomePage> {
-  List<AssetPathEntity> albumList = [];
+  List<_Album> albumList = [];
 
   @override
   void initState() {
     super.initState();
-    this.getAsset();
+    Future.delayed(Duration(milliseconds: 300), () {
+      this.getAsset();
+    });
   }
 
   Future getAsset() async {
@@ -24,9 +27,24 @@ class _SPHomePageState extends State<SPHomePage> {
     if (permissionRes != true) {
       return;
     }
-    albumList = await PhotoManager.getAssetPathList(hasVideo: true);
-    print('albumList');
-    print(albumList);
+
+    List<AssetPathEntity> albumListRes = await PhotoManager.getAssetPathList(
+        hasVideo: true);
+
+    List<_Album> tmpAlbum = [];
+    List<Future<List<AssetEntity>>> futureList = [];
+    albumListRes.forEach((album) {
+      futureList.add(album.assetList);
+    });
+
+    List<List<AssetEntity>> assetLists = await Future.wait(futureList);
+    for (int i = 0; i < assetLists.length; i++) {
+      tmpAlbum.add(new _Album(albumListRes[i].name, assetLists[i]));
+    }
+
+    this.setState(() {
+      albumList = tmpAlbum;
+    });
   }
 
   @override
@@ -45,6 +63,51 @@ class _SPHomePageState extends State<SPHomePage> {
   }
 
   Widget _buildItem(BuildContext context, int index) {
-    return Text(albumList[index].name);
+//    return Container(
+//      height: 100,
+//      color: Colors.green,
+//      child: Row(
+//        children: <Widget>[
+//          Text(albumList[index].name),
+//          Text(albumList[index].assetList.length.toString()),
+//        ],
+//      ),
+//    );
+    Future<Uint8List> thumbDataWithSize = albumList[index].assetList[albumList[index].assetList.length - 1]
+        .thumbDataWithSize(50, 50);
+    return FutureBuilder<Uint8List>(
+        future: thumbDataWithSize,
+        builder: (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.data != null) {
+            return Container(
+              height: 100,
+              color: Colors.green,
+              child: Row(
+                children: <Widget>[
+                  Image.memory(
+                    snapshot.data,
+                    fit: BoxFit.cover,
+                    width: 60,
+                    height: 60,
+                  ),
+                  Text(albumList[index].name),
+                  Text(albumList[index].assetList.length.toString()),
+                ],
+              ),
+            );
+          } else {
+            return Container();
+          }
+        }
+    );
   }
+}
+
+class _Album {
+  String name;
+  List<AssetEntity> assetList;
+
+  _Album(this.name, this.assetList);
+
 }
