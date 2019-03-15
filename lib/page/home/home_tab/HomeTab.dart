4ed64d57a1169341b0868f639f0acb534e1404fd
nell <326646683@qq.com>
2019-03-15 18:29:08 +0800

    继续首页

--- lib/page/home/home_tab/HomeTab.dart
+++ lib/page/home/home_tab/HomeTab.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as Math;
 import 'package:flutter/material.dart';
 import 'package:flutter_smallworld/common/utils/index.dart';
 
@@ -18,6 +19,12 @@ class HomeTab extends StatefulWidget {
 
 class _HomeTabState extends State<HomeTab> with AutomaticKeepAliveClientMixin {
   final ScrollController _scrollController = new ScrollController();
+  double startY;
+  int startTime;
+  double endY;
+  int endTime;
+  bool isCheck;
+  int currentIndex = 0;
 
   @override
   void initState() {
@@ -29,35 +36,96 @@ class _HomeTabState extends State<HomeTab> with AutomaticKeepAliveClientMixin {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-        onTapDown: (TapDownDetails details) {
-          debugPrint(details.globalPosition.toString());
-        },
-        onTapUp: (TapUpDetails details) {
-          debugPrint(details.globalPosition.toString());
-        },
-        child: ListView.builder(
-          controller: _scrollController,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: ScreenUtil
-                  .getInstance()
-                  .screenHeight,
+    return NotificationListener(
+      onNotification: (ScrollNotification note) {
+        print(note);
+        switch (note.runtimeType) {
+          case ScrollStartNotification:
+            if ((note as ScrollStartNotification).dragDetails != null) {
+              this.isCheck = true;
+//              print((note as ScrollStartNotification)
+//                  .dragDetails
+//                  .globalPosition
+//                  .dy);
+              this.startY = (note as ScrollStartNotification)
+                  .dragDetails
+                  .globalPosition
+                  .dy;
+              this.startTime = DateTime.now().millisecondsSinceEpoch;
+            }
+            break;
+          case ScrollUpdateNotification:
+            if ((note as ScrollUpdateNotification).dragDetails != null) {
+//              print((note as ScrollUpdateNotification)
+//                  .dragDetails
+//                  .globalPosition.dy);
+              this.endY = (note as ScrollUpdateNotification)
+                  .dragDetails
+                  .globalPosition
+                  .dy;
+            } else {
+              if (isCheck) {
+                print('dragDetails:isnull:' + 'isCheck:' + isCheck.toString());
+                // 滑动
+                this.isCheck = false;
+                autoScroll();
+              }
+//              print(DateTime.now().millisecondsSinceEpoch);
+            }
+            break;
+          case ScrollEndNotification:
+            if (isCheck) {
+              autoScroll();
+            }
+            break;
+        }
+      },
+      child: ListView.builder(
+        controller: _scrollController,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            decoration: BoxDecoration(border: SMCommonStyle.borderBottom03Gray),
+            height: ScreenUtil.getInstance().screenHeight,
+            child: GestureDetector(
+              onTap: (){
+                _scrollController
+                    .animateTo(2 * ScreenUtil.getInstance().screenHeight, duration: Duration(milliseconds: 300), curve: Curves.easeOut);
+              },
               child: Text('跳转到 $index'),
-            );
-          },
-        )
+            ),
+          );
+        },
+      ),
     );
   }
-//  child: GestureDetector(
-//  onTap: () {
-//  _scrollController.jumpTo(index * ScreenUtil
-//      .getInstance()
-//      .screenHeight);
-//  },
-//  child: Text('跳转到 $index'),
-//  ),
 
+  autoScroll() {
+    double dy = startY - endY;
+    this.endTime = DateTime.now().millisecondsSinceEpoch;
+    double v = dy.abs() / (this.endTime - this.startTime);
+
+    if (dy.abs() > ScreenUtil.getInstance().screenHeight / 3 || v > 0.5) {
+      print('移动');
+      if (dy > 0) {
+        currentIndex = currentIndex + 1;
+        _scrollController
+            .animateTo(currentIndex * ScreenUtil.getInstance().screenHeight, duration: Duration(milliseconds: 300), curve: Curves.easeOut);
+      } else {
+        currentIndex = currentIndex - 1;
+        if (currentIndex < 0) {
+          currentIndex = 0;
+        }
+        _scrollController
+            .animateTo(currentIndex * ScreenUtil.getInstance().screenHeight, duration: Duration(milliseconds: 300), curve: Curves.easeOut);
+      }
+    } else {
+      print('归位');
+      _scrollController
+          .animateTo(0 * ScreenUtil.getInstance().screenHeight, duration: Duration(milliseconds: 300), curve: Curves.easeOut);
+    }
+
+
+  }
 
   @override
   bool get wantKeepAlive => true;
