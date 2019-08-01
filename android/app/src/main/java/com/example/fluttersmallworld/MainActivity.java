c5e25853bb8b9f58cf20159cc4b892824c0ebb19
nell <nell@yunio.com>
2019-08-01 16:18:24 +0800

    flutter_forbidshot

--- android/app/src/main/java/com/example/fluttersmallworld/MainActivity.java
+++ android/app/src/main/java/com/example/fluttersmallworld/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fluttersmallworld;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -8,6 +9,13 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+//    if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+//      getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+//
+//
+//    } else {
+//      getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+//    }
     GeneratedPluginRegistrant.registerWith(this);
   }
 }
